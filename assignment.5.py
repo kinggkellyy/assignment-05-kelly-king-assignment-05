7375644198b9af81f371f57190bed1c4a467c8fe
kinggkellyy <kfking@loyola.edu>
2021-11-28 02:39:46 -0500

    cs1

--- assignment.5.py
+++ assignment.5.py
@@ -63,4 +63,11 @@ count_of_all_trips("2016-9-1")
 
 #parameters function
 def parameter(lat1, lon1, lat2, lon2):
-    distance = arccos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2)) * 3,959 miles.
+   distance = math.acos(math.sin(math.radians(lat1)) * math.sin(math.radians(lat2)) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(lon1-lon2))) * 3959
+   print(distance)
+parameter(+39.2904, -76.6122,+38.9072, -77.0369)
+
+def main (options):
+
+
+
